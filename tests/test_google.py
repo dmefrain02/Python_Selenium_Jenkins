024978fbb653b50b235f3c9356c843401ac88378
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2025-10-12 15:13:08 -0600

    Update test_google.py

--- tests/test_google.py
+++ tests/test_google.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 import xmlrunner
 
 class Test(unittest.TestCase):
