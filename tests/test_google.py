dcc425fac68db9eb47d35891667e97687ed10eac
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2025-10-12 15:11:54 -0600

    .

--- tests/test_google.py
+++ tests/test_google.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import xmlrunner
 
-class GoogleTest(unittest.TestCase):
+class Test(unittest.TestCase):
     def setUp(self):
         options = Options()
         prefs = {
@@ -25,6 +25,27 @@ class GoogleTest(unittest.TestCase):
     def test_title(self):
         self.driver.get("https://www.google.com")
         self.assertIn("Google", self.driver.title)
+
+    def test_mercado_libre(self):
+
+        options = Options()
+        prefs = {
+            "profile.default_content_settings.popups": 0,
+            "directory_upgrade":True
+        }
+        options.add_experimental_option("prefs",prefs)
+        options.add_argument('start-maximized')
+        #options.add_argument("--headless")
+
+        #self.driver = webdriver.Chrome(service=ChromeService("../Drivers/chromedriver.exe"), options=options)
+
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=options)
+        
+        self.driver.get("https://www.mercadolibre.co.cr/")
+        self.driver.find_element("xpath","//input[@id='cb1-edit']").send_keys("Play Station 5")
+        self.driver.find_element("xpath","//input[@id='cb1-edit']").send_keys(Keys.ENTER)
+        self.assertIn("Playstation 5 Pro 2tb Avenida Tecnologica", self.driver.find_element("xpath","//a[normalize-space()='Playstation 5 Pro 2tb Avenida Tecnologica']").text)
 
     def tearDown(self):
         self.driver.quit()
